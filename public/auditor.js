e633a792951ce0497b3c3dc6a2ea2ee88675cc44
Ryan Allred <Ryan@Allred.xyz>
2016-02-10 16:18:41 -0700

    Added score system for course audits.

--- public/auditor.js
+++ public/auditor.js
@@ -29,8 +29,9 @@ var Auditor = {}
 
       var finish = function(){
         audit(returned, function(errors){
+          var analysis = inspect(errors)
           if (callback)
-            callback(errors)
+            callback(errors, analysis)
         })
       }
 
@@ -117,19 +118,13 @@ var Auditor = {}
         var errors = Tester.exec(page,html)
 
         if (errors) {
-          console.warn(
-            'Error(s) Found:',
-            'https://byui.brightspace.com/d2l/le/content/'+page.orgUnitId+'/viewContent/'+page.id+'/View',
-            'https://byui.brightspace.com' + escape(page.url),
-             errors
-           )
           totalErrors.push({
             title: page.title,
             brightspace: 'https://byui.brightspace.com/d2l/le/content/'+page.orgUnitId+'/viewContent/'+page.id+'/View',
             contentpage: 'https://byui.brightspace.com' + escape(page.url),
             errors: errors
           })
-          callback(totalErrors) // concept: update as content is found
+          //callback(totalErrors) // concept: update as content is found
         }
 
         responded++
@@ -144,6 +139,45 @@ var Auditor = {}
           callback(totalErrors)
       })
     })
+  }
+
+  var inspect = function(report){
+    var points = 0,
+        totalPages = report.length,
+        urgentPages = 0,
+        brokenLinks = 0,
+        saturdays = 0
+
+    var countLinks = function(entry,name) {
+      if (entry.errors[name])
+        brokenLinks += entry.errors[name].count
+    }
+
+    var countSaturdays = function(entry,name) {
+      if (entry.errors[name])
+        saturdays += entry.errors[name].count
+    }
+
+    report.forEach(function(entry){
+      for(var name in entry.errors) {
+        points += entry.errors[name].points
+      }
+
+      countLinks(entry, 'IL2 Link')
+      countLinks(entry, 'Box Link')
+      countLinks(entry, 'Benjamin Link')
+      countLinks(entry, 'Static IL3 Link')
+
+      countSaturdays(entry, 'Mentions of Saturdays')
+    })
+
+    return {
+      points: points,
+      totalPages: totalPages,
+      urgentPages: urgentPages,
+      brokenLinks: brokenLinks,
+      saturdays: saturdays
+    }
   }
 
   var Tester = {
@@ -179,29 +213,30 @@ var Auditor = {}
       return Tester.errors;
     },
 
-    mark: function(name,count){
-      Tester.errors[name] = Tester.errors[name] || 0
-      Tester.errors[name] += count
+    mark: function(name,points,count){
+      Tester.errors[name] = Tester.errors[name] || {count: 0, points: 0}
+      Tester.errors[name].count += count
+      Tester.errors[name].points += points
     },
 
-    addRaw: function(name,fn){
+    addRaw: function(name, points, fn){
       Tester.rawChecks.push(function(page,html) {
         var result = fn(page,html)
         if (result) {
-          Tester.mark(name,Number(result))
+          Tester.mark(name,points,Number(result))
         }
       })
 
       return Tester;
     },
 
-    add: function(name, query, fn){
+    add: function(name, points, query, fn){
       Tester.tests[query] = Tester.tests[query] || []
 
       Tester.tests[query].push(function(input) {
         var result = fn(input)
         if (result) {
-          Tester.mark(name,Number(result))
+          Tester.mark(name,points,Number(result))
         }
       })
 
@@ -210,7 +245,7 @@ var Auditor = {}
   }
 
   Tester
-    .add(undefined,'img',function(ele){
+    .add(undefined,0,'img',function(ele){
       // This is not a test. It changes images so they don't load 404 errors.
 
       ele.attr('data-src', ele.attr('src'))
@@ -219,58 +254,58 @@ var Auditor = {}
       return false
     })
     // return true on error found
-    .add('IL2 Link','a',function(ele){
+    .add('IL2 Link', 10,'a',function(ele){
       return ele.attr('href') &&
              ele.attr('href').indexOf('brainhoney') >= 0
     })
-    .add('Box Link','a',function(ele){
+    .add('Box Link', 10, 'a',function(ele){
       return ele.attr('href') &&
              ele.attr('href').indexOf('box.com') >= 0
     })
-    .add('Benjamin Link','a',function(ele){
+    .add('Benjamin Link', 10,'a',function(ele){
       return ele.attr('href') &&
              ele.attr('href').indexOf('courses.byui.edu') >= 0
     })
-    .add('Static IL3 Link','a',function(ele){
+    .add('Static IL3 Link', 10,'a',function(ele){
       return ele.attr('href') &&
             (ele.attr('href').indexOf('/calendar') >= 0 ||
              ele.attr('href').indexOf('/home') >= 0 ||
              ele.attr('href').indexOf('/viewContent') >= 0)
     })
-    .add('Empty Links','a',function(ele){
+    .add('Empty Links',8,'a',function(ele){
       return !ele.attr('href') && !ele.attr('id')
     })
-    .add('Empty Images','img',function(ele){
+    .add('Empty Images',8,'img',function(ele){
       return !ele.attr('data-src')
     })
-    .add('IL2 Images','img',function(ele){
+    .add('IL2 Images',10,'img',function(ele){
       return ele.attr('href') &&
              ele.attr('data-src').indexOf('brainhoney') >= 0
     })
-    .add('Bad Image Widths','img',function(ele){
+    .add('Bad Image Widths',2,'img',function(ele){
       if (!ele.attr('width'))
         return true
 
       return !ele.attr('width').endsWith('%')
     })
-    .add('Image Alt Text','img',function(ele){
+    .add('Image Alt Text',4,'img',function(ele){
       return !ele.attr('alt')
     })
-    .add('Spans','span',function(ele){
+    .add('Spans',1,'span',function(ele){
       return true
     })
-    .add('Depricated Tags','b, i, br',function(ele){
+    .add('Depricated Tags',1,'b, i, br',function(ele){
       return true
     })
-    .addRaw('Mentions of Saturdays',function(page, html){
+    .addRaw('Mentions of Saturdays',10,function(page, html){
       var arr = (html).match(/saturday/gi)
       return arr ? arr.length : false
     })
-    .addRaw('IL2 Variables',function(page, html){
+    .addRaw('IL2 Variables',10,function(page, html){
       var arr = (html).match(/\$[A-Za-z]+\S\$/g)
       return arr ? arr.length : false
     })
-    .addRaw('HTML Title',function(page, html){
+    .addRaw('HTML Title',6,function(page, html){
       var title = (/<title[^>]*>(.*?)<\/title>/g).exec(html)
       if (!title) return true
       else title = title[1]
--- public/tags.js
+++ public/tags.js
@@ -1,12 +1,16 @@
 'use strict';
 
-riot.tag2('app', '<div class="entry" each="{entry in this.audit}"> <h2>{entry.title}</h2> <a href="{entry.brightspace}" target="_blank">Brightspace</a> <a href="{entry.contentpage}" target="_blank">Content Page</a> <div class="errors"> <span class="error" each="{name, count in entry.errors}"><strong>{count}</strong> {name}</span> </div> </div>', '.entry h2 { margin-bottom: 10px; } .entry:not(:first-child) { border-top: 1px solid #ccc; } .entry .errors { border-radius: 3px; box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.1) inset; background: #f5ff6a; padding: 8px 20px; }', '', function (opts) {
+riot.tag2('app', '<div if="{!this.audit}">Loading... hold on...</div> <div if="{this.audit}"> <h1>Score: {this.audit.analysis.score}</h1> </div> <div class="entry" each="{entry in this.audit}" onclick="{this.open}"> <h2>{entry.title}</h2> <div class="meta"> <a href="{entry.brightspace}" target="_blank">Brightspace</a> <a href="{entry.contentpage}" target="_blank">Content Page</a> <div class="errors"> <span class="error" each="{name, count in entry.errors}"><strong>{count}</strong> {name}</span> </div> </div> </div>', '.entry h2 { margin-bottom: 10px; font-weight: normal; font-size: 1.5rem; } .entry:not(:first-child) { border-top: 1px solid #ccc; } .entry .meta { display: none; } .entry .errors { border-radius: 3px; box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.1) inset; background: #f5ff6a; padding: 8px 20px; display: inline-block; } .entry .errors .error { display: inline-block; padding: 0px 20px; }', '', function (opts) {
   var _this = this;
 
   var courseId = Number(window.location.href.split('/')[5].split('-')[0]),
       errors = [];
 
-  Auditor.checkCourse(courseId, function (audit) {
-    _this.update({ audit: audit });
+  Auditor.checkCourse(courseId, function (audit, analysis) {
+    _this.update({ audit: audit, analysis: analysis });
   });
+
+  this.open = function (e) {
+    $(e.target).find('meta').toggle();
+  };
 }, '{ }');
